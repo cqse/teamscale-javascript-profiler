bbe8f540446af61ee01469496a26aa7400c64315
Andreas Stahlbauer <stahlbauer@cqse.eu>
2022-01-18 08:52:36 +0100

    TS-28121 Missing docs added

--- packages/teamscale-coverage-collector/src/utils/QueryParameters.ts
+++ packages/teamscale-coverage-collector/src/utils/QueryParameters.ts
@@ -1,12 +1,21 @@
+/**
+ * Helper class for building HTTP query parameter strings.
+ */
 export default class QueryParameters {
 	private parameters: Record<string, string> = {};
 
+	/**
+	 * Adds a parameter if the value is defined.
+	 */
 	public addIfDefined(key: string, value: string | undefined): void {
 		if (value) {
 			this.parameters[key] = value;
 		}
 	}
 
+	/**
+	 * Convert the parameters to a query string that can be added to an URL.
+	 */
 	public toQueryParamString(): string {
 		return Object.entries(this.parameters)
 			.map(pair => pair.map(encodeURIComponent).join('='))

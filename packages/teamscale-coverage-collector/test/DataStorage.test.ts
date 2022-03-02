0bca7ae8272715a69654f43e010bd640153c6192
Andreas Stahlbauer <stahlbauer@cqse.eu>
2022-03-02 14:37:31 +0100

    TS-29512 Added a missing test

---
+++ packages/teamscale-coverage-collector/test/DataStorage.test.ts
@@ -0,0 +1,16 @@
+import { ProjectCoverage } from '../src/storage/DataStorage';
+
+test('Test if coverage is aggregated in the storage', () => {
+	const storage = new ProjectCoverage('42');
+	storage.putLine('main.ts', 1);
+	storage.putLine('main.ts', 2);
+	storage.putLine('main.ts', 4);
+	const lines = new Set();
+	for (const coverage of storage.getCoverage()) {
+		coverage.coveredLines.forEach(l => lines.add(l));
+	}
+	expect(lines).toContain(1);
+	expect(lines).toContain(2);
+	expect(lines).toContain(4);
+	expect(lines).not.toContain(3);
+});

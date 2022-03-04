2627dbc285bd6df162e7639c24360d3dd920bdbd
Andreas Stahlbauer <stahlbauer@cqse.eu>
2022-03-04 13:45:14 +0100

    TS-29551 Rework: More tests

--- packages/teamscale-javascript-instrumenter/test/Task.test.ts
+++ packages/teamscale-javascript-instrumenter/test/Task.test.ts
@@ -1,19 +1,37 @@
 import { OriginSourcePattern } from '../src/instrumenter/Task';
 
-test('Test include pattern without extension', () => {
+test('Include pattern without extension', () => {
 	const pattern = new OriginSourcePattern('./src/app/**/*', undefined);
 	const result = pattern.isAnyIncluded(['./src/app/app.components.ts', './src/app/messages/messages.component.ts']);
 	expect(result).toBeTruthy();
 });
 
-test('Test include pattern with extension', () => {
+test('Include pattern with extension', () => {
 	const pattern = new OriginSourcePattern('src/app/**/*.ts', undefined);
 	const result = pattern.isAnyIncluded(['src/app/app.components.ts', 'src/app/messages/messages.component.ts']);
 	expect(result).toBeTruthy();
 });
 
-test('Test include pattern with extension. Path normalization', () => {
+test('Include pattern with extension. Path normalization', () => {
 	const pattern = new OriginSourcePattern('src/app/**/*.ts', undefined);
 	const result = pattern.isAnyIncluded(['./src/app/app.components.ts', './src/app/messages/messages.component.ts']);
 	expect(result).toBeTruthy();
+});
+
+test('Exclude pattern with extension. Path normalization', () => {
+	const pattern = new OriginSourcePattern(undefined, 'src/app/**/*.ts');
+	expect(
+		pattern.isAnyIncluded(['./berg/app/app.components.ts', './src/app/messages/messages.component.ts'])
+	).toBeTruthy();
+	expect(pattern.isAnyIncluded(['./berg/app/app.components.ts'])).toBeTruthy();
+	expect(pattern.isAnyIncluded(['./src/app/app.components.ts'])).toBeFalsy();
+});
+
+test('Exclude and pattern', () => {
+	const pattern = new OriginSourcePattern('src/bar/**/*.ts', 'src/foo/**/*.ts');
+	expect(
+		pattern.isAnyIncluded(['./src/bar/app.components.ts', './src/foo/messages/messages.component.ts'])
+	).toBeTruthy();
+	expect(pattern.isAnyIncluded(['./src/foo/messages/messages.component.ts'])).toBeFalsy();
+	expect(pattern.isAnyIncluded(['./src/bar/messages/messages.component.ts'])).toBeTruthy();
 });

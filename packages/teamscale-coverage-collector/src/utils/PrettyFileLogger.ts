20092426965a8b8bf4b42772a6d989c433129866
Eric Paul <eric.paul.std@gmail.com>
2022-04-26 09:38:16 +0200

    TS-29492 Add documentation

--- packages/teamscale-coverage-collector/src/utils/PrettyFileLogger.ts
+++ packages/teamscale-coverage-collector/src/utils/PrettyFileLogger.ts
@@ -2,20 +2,21 @@ import Logger from 'bunyan';
 import 'dotenv/config';
 import { WriteStream } from 'fs';
 
+/** Class for log4j-like logger. */
 export class PrettyFileLogger {
-	fileOutputStream: WriteStream;
+	outputStream: WriteStream;
 
 	constructor(outputStream: WriteStream) {
-		this.fileOutputStream = outputStream;
+		this.outputStream = outputStream;
 	}
 
 	public write(rec: Record<any, any>): void {
-		this.fileOutputStream.write(
+		this.outputStream.write(
 			`[${rec.time.toISOString()}] ${Logger.nameFromLevel[rec.level].toUpperCase()}: ${rec.msg}\n`
 		);
 	}
 
 	public end(): void {
-		this.fileOutputStream.close();
+		this.outputStream.close();
 	}
 }
--- packages/teamscale-coverage-collector/src/utils/StdConsoleLogger.ts
+++ packages/teamscale-coverage-collector/src/utils/StdConsoleLogger.ts
@@ -1,6 +1,7 @@
 import Logger from 'bunyan';
 import 'dotenv/config';
 
+/** Class for console logger. */
 export class StdConsoleLogger {
 	public write(rec: Record<any, any>): void {
 		console.log(`[${rec.time.toISOString()}] ${Logger.nameFromLevel[rec.level].toUpperCase()}: ${rec.msg}`);

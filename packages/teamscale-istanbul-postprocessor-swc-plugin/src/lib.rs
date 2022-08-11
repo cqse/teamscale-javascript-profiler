fb738dff750c8bc1ae8d2278a9e50dfb97e52042
Andreas Stahlbauer <stahlbauer@cqse.eu>
2022-08-11 17:28:01 +0200

    TS-30325 WiP

--- packages/teamscale-istanbul-postprocessor-swc-plugin/src/lib.rs
+++ packages/teamscale-istanbul-postprocessor-swc-plugin/src/lib.rs
@@ -1,9 +1,21 @@
 use std::sync::Arc;
-use swc_coverage_instrument::{InstrumentOptions};
-use swc_plugin::{ast::*, plugin_transform, TransformPluginProgramMetadata};
 use swc_common::comments::NoopComments;
 use swc_common::SourceMap;
+use swc_coverage_instrument::InstrumentOptions;
+use swc_common::{chain, pass::Optional, FileName};
+use swc_ecmascript::visit::{noop_fold_type, Fold, as_folder};
+use swc_ecma_ast::UpdateOp;
+use swc_ecmascript::visit::{VisitMut, VisitMutWith};
+use swc_core::{
+    ast::Program,
+    plugin::{
+        plugin_transform,
+        proxies::TransformPluginProgramMetadata,
+    },
+    visit::FoldWith,
+};
 
+#[derive(Debug)]
 pub struct TransformVisitor;
 
 impl VisitMut for TransformVisitor {
@@ -13,38 +25,21 @@ impl VisitMut for TransformVisitor {
 
     fn visit_mut_update_op(&mut self, n: &mut UpdateOp) {
         // dbg!(self);
-        n.visit_mut_children_with(self);
+        n.visit_mut_children_with(self);  
     }
 }
 
-/// An example plugin function with macro support.
-/// `plugin_transform` macro interop pointers into deserialized structs, as well
-/// as returning ptr back to host.
-///
-/// It is possible to opt out from macro by writing transform fn manually via
-/// `__transform_plugin_process_impl(
-///     ast_ptr: *const u8,
-///     ast_ptr_len: i32,
-///     config_str_ptr: *const u8,
-///     config_str_ptr_len: i32,
-///     context_str_ptr: *const u8,
-///     context_str_ptr_len: i32) ->
-///     i32 /*  0 for success, fail otherwise.
-///             Note this is only for internal pointer interop result,
-///             not actual transform result */
-///
-/// if plugin need to handle low-level ptr directly. However, there are
-/// important steps manually need to be performed like sending transformed
-/// results back to host. Refer swc_plugin_macro how does it work internally.
-#[plugin_transform]
-pub fn process_transform(program: Program, _metadata: TransformPluginProgramMetadata) -> Program {
-    let visitor = swc_coverage_instrument::create_coverage_instrumentation_visitor(
-        Arc::new(SourceMap::default()),
-        NoopComments {},
-        InstrumentOptions::default(),
-        String::from("Hello.js"),
-    );
+pub fn transformer() -> impl Fold + VisitMut {
+    as_folder(TransformVisitor {})
+}
 
-    let instrumented = program.fold_with(&mut as_folder(visitor));
-    return instrumented.fold_with(&mut as_folder(TransformVisitor));
+#[plugin_transform]
+pub fn process_transform(mut program: Program, data: TransformPluginProgramMetadata) -> Program {
+    // See https://github.com/vercel/next.js/tree/canary/packages/next-swc/crates/styled_components as an example.
+
+    let mut pass = transformer();    
+    
+    program.visit_mut_with(&mut pass);
+
+    program
 }
--- packages/teamscale-istanbul-postprocessor-swc-plugin/tests/maintest.rs
+++ packages/teamscale-istanbul-postprocessor-swc-plugin/tests/maintest.rs
@@ -1,18 +1,26 @@
-use std::path::PathBuf;
+#![deny(unused)]
 
-use swc_ecma_transforms_testing::{test, test_fixture};
-use swc_ecma_visit::{as_folder};
-use teamscale_istanbul_postprocessor_swc_plugin::TransformVisitor;
+use std::{path::PathBuf};
+use swc_ecma_transforms_testing::test_fixture;
+use swc_ecmascript::{
+    parser::{EsConfig, Syntax}
+};
 
-#[testing::fixture("tests/fixtures/test1/input.js")]
-fn fixture_test1(input_path: PathBuf) {
-    let fixture_path = input_path.parent().unwrap();
-    let output_path = fixture_path.join("output.js");
+use teamscale_istanbul_postprocessor_swc_plugin::{transformer};
+
+#[testing::fixture("tests/fixtures/**/input.js")]
+fn fixture(input: PathBuf) {
+    let test_dir = input.parent().unwrap();
 
     test_fixture(
-        Default::default(),
-        &|_t| as_folder(TransformVisitor),
+        Syntax::Es(EsConfig {
+            jsx: true,
+            ..Default::default()
+        }),
+        &|_| {        
+            transformer()
+        },
         &input,
-        &output_path,
-    );
+        &test_dir.join("output.js"),
+    )
 }

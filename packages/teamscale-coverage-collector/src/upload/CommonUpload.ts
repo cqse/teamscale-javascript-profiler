5e92fe8b317329b4fe02e8b6d90273eac5c38d3e
Andreas Punz <punz@cqse.eu>
2022-10-13 10:16:58 +0200

    TS-29873 Update changelog

--- packages/teamscale-coverage-collector/src/upload/CommonUpload.ts
+++ packages/teamscale-coverage-collector/src/upload/CommonUpload.ts
@@ -1,72 +1,64 @@
-import FormData from "form-data";
-import fs from "fs";
-import Logger from "bunyan";
-import {AxiosRequestConfig, AxiosResponse} from "axios";
-import {inspect} from "util";
+import FormData from 'form-data';
+import fs from 'fs';
+import Logger from 'bunyan';
+import { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { inspect } from 'util';
 
 /**
  * Error that is thrown when the upload failed
  */
 export class UploadError extends Error {
-    // No special fields needed compared to Error.
-    // Sole use is to be able to distinguish upload errors from other errors.
+	// No special fields needed compared to Error.
+	// Sole use is to be able to distinguish upload errors from other errors.
 }
 
 /**
- * The class providing common functionality to upload reports to Teamscale or external artifact storages like artifactory.
+ * Prepares the form data from a given configuration file for the upload.
  */
-export class CommonUpload {
+export function prepareFormData(coverageFile: string): FormData {
+	const form = new FormData();
+	form.append('report', fs.createReadStream(coverageFile), 'coverage.simple');
+	return form;
+}
 
-    /**
-     * Prepares the form data from a given configuration file for the upload.
-     */
-    public static prepareFormData(coverageFile: string): FormData {
-        const form = new FormData();
-        form.append('report', fs.createReadStream(coverageFile), 'coverage.simple');
-        return form;
-    }
+/**
+ * Uploads a coverage file with the provided configuration.
+ */
+export async function performUpload(
+	url: string,
+	form: FormData,
+	config: AxiosRequestConfig<FormData>,
+	uploadFunction: <T = any, R = AxiosResponse<T>, D = FormData>(
+		url: string,
+		data?: D,
+		config?: AxiosRequestConfig<D>
+	) => Promise<R>,
+	logger: Logger
+): Promise<void> {
+	try {
+		const response = await uploadFunction(url, form, config);
+		logger.info(`Upload finished with code ${response.status}.`);
+	} catch (error: any) {
+		if (error.response) {
+			const response = error.response;
+			if (response.status >= 400) {
+				throw new UploadError(
+					`Upload failed with code ${response.status}: ${response.statusText}. Response Data: ${response.data}`
+				);
+			} else {
+				logger.info(`Upload with status code ${response.status} finished.`);
+			}
+		} else if (error.request) {
+			throw new UploadError(`Upload request did not receive a response.`);
+		}
 
-    /**
-     * Uploads a coverage file with the provided configuration.
-     */
-    public static async performUpload(
-        url: string,
-        form: FormData,
-        config: AxiosRequestConfig<FormData>,
-        uploadFunction: <T = any, R = AxiosResponse<T>, D = FormData>(url: string, data?: D, config?: AxiosRequestConfig<D>) => Promise<R>,
-        logger: Logger
-    ): Promise<void> {
-        try {
-            const response = await uploadFunction(
-                url,
-                form,
-                config
-            )
-            logger.info(`Upload finished with code ${response.status}.`);
-        } catch (error: any) {
-            if (error.response) {
-                const response = error.response;
-                if (response.status >= 400) {
-                    throw new UploadError(
-                        `Upload failed with code ${response.status}: ${response.statusText}. Response Data: ${response.data}`
-                    );
-                } else {
-                    logger.info(`Upload with status code ${response.status} finished.`);
-                }
-            } else if (error.request) {
-                throw new UploadError(`Upload request did not receive a response.`);
-            }
-
-            if (error.message) {
-                logger.debug(
-                    `Something went wrong when uploading data: ${error.message}. Details of the error: ${inspect(
-                        error
-                    )}`
-                );
-                throw new UploadError(`Something went wrong when uploading data: ${error.message}`);
-            } else {
-                throw new UploadError(`Something went wrong when uploading data: ${inspect(error)}`);
-            }
-        }
-    }
+		if (error.message) {
+			logger.debug(
+				`Something went wrong when uploading data: ${error.message}. Details of the error: ${inspect(error)}`
+			);
+			throw new UploadError(`Something went wrong when uploading data: ${error.message}`);
+		} else {
+			throw new UploadError(`Something went wrong when uploading data: ${inspect(error)}`);
+		}
+	}
 }

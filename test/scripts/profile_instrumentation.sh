8dddc5111ef1f17872be8d022102cf8c81ed212b
Andreas Stahlbauer <stahlbauer@cqse.eu>
2022-11-22 17:52:09 +0100

    TS-32321 Initial script adjustments for performance benchmarking

---
+++ test/scripts/profile_instrumentation.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ ! -f "tsconfig.vaccine.json" ]
+then
+    echo "Please run the script from the root of the instrumenter package."
+    exit 1
+fi
+
+DIST_DIR="$1"
+if [ -z $DIST_DIR ]
+then
+    echo "Please provide a distribution folder to instrument."
+    exit 1
+fi
+
+COLLECTOR_PORT="$2"
+if [ -z $COLLECTOR_PORT ]
+then
+    echo "Please provide a collector port as second argument."
+    exit 1
+fi
+
+OUTPUT_FILE="$3"
+if [ -z $OUTPUT_FILE ]
+then
+    echo "Please provide a target file name."
+    exit 1
+fi
+
+MEMORY_SAMPLING_FILE="memory-sampling-instrument.dat"
+
+# Run the instrumenter with the memory profiler attached
+START_NANOS=$(date +%s%N)
+cd packages/teamscale-javascript-instrumenter
+mprof run --interval 0.05 --include-children --output "$MEMORY_SAMPLING_FILE" \
+    node ./dist/src/main.js \
+        --in-place ${DIST_DIR} \
+        --collector "ws://localhost:$COLLECTOR_PORT" \
+        "${@:4}"
+END_NANOS=$(date +%s%N)
+
+# Collect and compute the results
+DURATION_MILLIS=$((($END_NANOS - $START_NANOS) / 1000000))
+PEAK_MEM_MB=$(sed 1d $MEMORY_SAMPLING_FILE | cut -f3 -d" " | sort -n | tail -n1)
+
+# Write the results to a JSON file
+echo "{ \"duration_millis\": $DURATION_MILLIS, \"memory_mb_peak\": $PEAK_MEM_MB }" > $OUTPUT_FILE
---
+++ test/scripts/profile_testing.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# We assume that this script is executed from the
+# root of the JS Profiler project.
+
+if [ ! -f "package.json" ]
+then
+    echo "Please run the script from the root of the repository."
+    exit 1
+fi
+
+STUDY_NAME="$1"
+if [ -z $STUDY_NAME ]
+then
+    echo "Please provide a study name as first argument."
+    exit 1
+fi
+
+SERVER_PORT="$2"
+if [ -z $SERVER_PORT ]
+then
+    echo "Please provide a server port as second argument."
+    exit 1
+fi
+
+OUTPUT_FILE="$3"
+if [ -z $OUTPUT_FILE ]
+then
+    echo "Please provide a target file name."
+    exit 1
+fi
+
+BASE_URL="http://localhost:${SERVER_PORT}"
+INTEGRATION_FOLDER="test/integration/${STUDY_NAME}/"
+MEMORY_SAMPLING_FILE="memory-sampling-testing.dat"
+
+# Run the test with the memory profiler attached
+START_NANOS=$(date +%s%N)
+mprof run --interval 0.05 --include-children --output "$MEMORY_SAMPLING_FILE" \
+    npx cypress run \
+        --config-file "cypress.json" \
+        --reporter "junit" \
+        --browser "chrome" \
+        --headless \
+        --quiet \
+        --config "{ \"baseUrl\": \"${BASE_URL}\", \"video\": false, \"integrationFolder\": \"${INTEGRATION_FOLDER}\" }" 
+END_NANOS=$(date +%s%N)
+
+# Collect and compute the results
+DURATION_MILLIS=$((($END_NANOS - $START_NANOS) / 1000000))
+PEAK_MEM_MB=$(sed 1d $MEMORY_SAMPLING_FILE | cut -f3 -d" " | sort -n | tail -n1)
+
+# Write the results to a JSON file
+echo "{ \"duration_millis\": $DURATION_MILLIS, \"memory_mb_peak\": $PEAK_MEM_MB }" > $OUTPUT_FILE

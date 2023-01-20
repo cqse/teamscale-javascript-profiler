82c9e7fd03f5c5320dc6e37854b9e0618a375f09
Andreas Stahlbauer <stahlbauer@cqse.eu>
2023-01-20 15:25:33 +0100

    TS-32321 Replaced 'mprof' by '/usr/bin/time' and some cleanup

--- test/scripts/profile_instrumentation.sh
+++ test/scripts/profile_instrumentation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source $SCRIPT_DIR/common.sh.inc
+source "$SCRIPT_DIR"/common.sh.inc
 
 if [ ! -f "tsconfig.vaccine.json" ]
 then
@@ -10,44 +10,42 @@ then
 fi
 
 DIST_DIR="$1"
-if [ -z $DIST_DIR ]
+if [ -z "$DIST_DIR" ]
 then
     echo "Please provide a distribution folder to instrument."
     exit 1
 fi
 
 COLLECTOR_PORT="$2"
-if [ -z $COLLECTOR_PORT ]
+if [ -z "$COLLECTOR_PORT" ]
 then
     echo "Please provide a collector port as second argument."
     exit 1
 fi
 
 OUTPUT_FILE="$3"
-if [ -z $OUTPUT_FILE ]
+if [ -z "$OUTPUT_FILE" ]
 then
     echo "Please provide a target file name."
     exit 1
 fi
 
-MEMORY_SAMPLING_FILE="memory-sampling-instrument.dat"
-if [ -f $MEMORY_SAMPLING_FILE ]
+PROFILING_RESULTS_FILE="bench-perf-stats.dat"
+if [ -f $PROFILING_RESULTS_FILE ]
 then
-  rm $MEMORY_SAMPLING_FILE
+  rm $PROFILING_RESULTS_FILE
 fi
 
 # Run the instrumenter with the memory profiler attached
-START_NANOS=$(gnudate +%s%N)
-mprof run --interval 0.01 --include-children --output "$MEMORY_SAMPLING_FILE" \
+/usr/bin/time -o "$PROFILING_RESULTS_FILE"  -f "%M %e" \
     node ./dist/src/main.js \
-        --in-place ${DIST_DIR} \
+        --in-place "${DIST_DIR}" \
         --collector "ws://localhost:$COLLECTOR_PORT" \
         "${@:4}"
-END_NANOS=$(gnudate +%s%N)
 
 # Collect and compute the results
-DURATION_MILLIS=$((($END_NANOS - $START_NANOS) / 1000000))
-PEAK_MEM_MB=$(sed 1d $MEMORY_SAMPLING_FILE | cut -f2 -d" " | sort -n | tail -n1)
+DURATION_SECS=$(cut -f2 -d" " < $PROFILING_RESULTS_FILE)
+PEAK_MEM_MB=$(expr "$(cut -f1 -d" " < $PROFILING_RESULTS_FILE)" / 1024)
 
 # Write the results to a JSON file
-echo "{ \"duration_millis\": $DURATION_MILLIS, \"memory_mb_peak\": $PEAK_MEM_MB }" > $OUTPUT_FILE
+echo "{ \"duration_secs\": $DURATION_SECS, \"memory_mb_peak\": $PEAK_MEM_MB }" > $OUTPUT_FILE
--- test/scripts/profile_testing.sh
+++ test/scripts/profile_testing.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source $SCRIPT_DIR/common.sh.inc
+source "$SCRIPT_DIR"/common.sh.inc
 
 # We assume that this script is executed from the
 # root of the JS Profiler project.
@@ -13,21 +13,21 @@ then
 fi
 
 STUDY_NAME="$1"
-if [ -z $STUDY_NAME ]
+if [ -z "$STUDY_NAME" ]
 then
     echo "Please provide a study name as first argument."
     exit 1
 fi
 
 SERVER_PORT="$2"
-if [ -z $SERVER_PORT ]
+if [ -z "$SERVER_PORT" ]
 then
     echo "Please provide a server port as second argument."
     exit 1
 fi
 
 OUTPUT_FILE="$3"
-if [ -z $OUTPUT_FILE ]
+if [ -z "$OUTPUT_FILE" ]
 then
     echo "Please provide a target file name."
     exit 1
@@ -36,27 +36,25 @@ fi
 BASE_URL="http://localhost:${SERVER_PORT}"
 INTEGRATION_FOLDER="test/integration/${STUDY_NAME}/"
 
-MEMORY_SAMPLING_FILE="memory-sampling-instrument.dat"
-if [ -f $MEMORY_SAMPLING_FILE ]
+PROFILING_RESULTS_FILE="bench-perf-stats.dat"
+if [ -f $PROFILING_RESULTS_FILE ]
 then
-  rm $MEMORY_SAMPLING_FILE
+  rm $PROFILING_RESULTS_FILE
 fi
 
 # Run the test with the memory profiler attached
-START_NANOS=$(gnudate +%s%N)
-mprof run --interval 0.01 --include-children --output "$MEMORY_SAMPLING_FILE" \
+/usr/bin/time -o "$PROFILING_RESULTS_FILE"  -f "%M %e" \
     npx cypress run \
         --config-file "cypress.json" \
         --reporter "junit" \
         --browser "chrome" \
         --headless \
         --quiet \
-        --config "{ \"baseUrl\": \"${BASE_URL}\", \"video\": false, \"integrationFolder\": \"${INTEGRATION_FOLDER}\" }" 
-END_NANOS=$(gnudate +%s%N)
+        --config "{ \"baseUrl\": \"${BASE_URL}\", \"video\": false, \"integrationFolder\": \"${INTEGRATION_FOLDER}\" }"
 
 # Collect and compute the results
-DURATION_MILLIS=$((($END_NANOS - $START_NANOS) / 1000000))
-PEAK_MEM_MB=$(sed 1d $MEMORY_SAMPLING_FILE | cut -f2 -d" " | sort -n | tail -n1)
+DURATION_SECS=$(cut -f2 -d" " < $PROFILING_RESULTS_FILE)
+PEAK_MEM_MB=$(expr "$(cut -f1 -d" " < $PROFILING_RESULTS_FILE)" / 1024)
 
 # Write the results to a JSON file
-echo "{ \"duration_millis\": $DURATION_MILLIS, \"memory_mb_peak\": $PEAK_MEM_MB }" > $OUTPUT_FILE
+echo "{ \"duration_secs\": $DURATION_SECS, \"memory_mb_peak\": $PEAK_MEM_MB }" > $OUTPUT_FILE

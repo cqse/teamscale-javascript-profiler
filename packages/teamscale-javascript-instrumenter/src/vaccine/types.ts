8538e7052073cd68d48e63b65574b3e6d72242e4
Fabian Streitel <streitel@cqse.eu>
2024-02-13 14:36:49 +0100

    fix Teamscale findings

--- packages/teamscale-javascript-instrumenter/src/vaccine/types.ts
+++ packages/teamscale-javascript-instrumenter/src/vaccine/types.ts
@@ -6,19 +6,29 @@ export type CoveredRanges = {
 	lines: number[]
 };
 
+/**
+ * The collector URL is known at build time.
+ */
 export interface CollectorSpecifierUrl {
+	/** Type of collector specifier. */
 	type: "url";
+	/** The URL under which the collector can be reached at runtime. */
 	url: string;
 }
 
 /**
- * The collector can be reached by replacing a term in document.location.hostname and optionally changing the port.
+ * The collector can be reached by replacing a term in location.host and optionally changing the port.
  */
 export interface CollectorSpecifierSubstitutionPattern {
+	/** Type of collector specifier. */
 	type: "substitutionPattern";
+	/** Literal string to search for in location.host. */
 	search: string;
+	/** Literal replacement string. */
 	replace: string;
+	/** Optional port. If no port is given, uses the protocol's default port. */
 	port?: number;
+	/** Whether to use ws or wss protocol. */
 	useWss: boolean;
 }
 
